c3e1cc880d44657b8abc492b2303e5395ed65581
Alexandre Vieira <AlexandreV1@bitbucket.org>
2020-08-28 05:04:04 -0300

    added new frontend

--- frontend/writefy/src/screen/indexScreen.js
+++ frontend/writefy/src/screen/indexScreen.js
@@ -1,6 +1,27 @@
 import React from "react";
 import Menu from "../component/leftBar.js";
 import styles from "./mainContainer.module.css";
+function SmallBook(props) {
+  return (
+    <>
+      {" "}
+      <div style={{ display: "flex", marginTop: "10px" }}>
+        <img src={props.image} className={styles.smallBook} />
+        <div style={{ display: "block" }}>
+          <p className={styles.smallBookTitle}>{props.title}</p>
+          <p className={styles.achievementBook}>
+            {" "}
+            You've completed{" "}
+            <span className={styles.percent}>{props.percent}%</span> of this
+            book
+          </p>
+          <button className={styles.buttonRead}>Read more</button>
+        </div>
+      </div>
+      <div className={styles.horizontalDivider}></div>
+    </>
+  );
+}
 export default function App(props) {
   return (
     <div style={{ display: "flex" }}>
@@ -39,7 +60,7 @@ export default function App(props) {
           </div>
 
           <div style={{ marginLeft: "5px" }}>
-            <p className={styles.titleForImage}>Vanished into the woods</p>
+            <p className={styles.titleForImage}>Book Title</p>
 
             <div className={styles.boxBehind}></div>
             <img
@@ -51,7 +72,7 @@ export default function App(props) {
             <p className={styles.authorForImage}>@ John doe</p>
 
             <img
-              src={require("../assets/book_example_1.jpg")}
+              src={require("../assets/book_example_2.jpg")}
               className={styles.boxContainer}
             />
           </div>
@@ -113,6 +134,33 @@ export default function App(props) {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.lineDivider}></div>
+
+      <div className={styles.sectionTwoContainer}>
+        <p style={{ color: "#545454" }} className={styles.mainTitle}>
+          Continue{" "}
+          <span style={{ fontWeight: "bold" }}>
+            Read<span style={{ color: "orange" }}>ing </span>...
+          </span>
+        </p>
+        <div>
+          <SmallBook
+            title="The Arrivals"
+            percent={54}
+            image={require("../assets/example_3.jpeg")}
+          />
+          <SmallBook
+            title="Story Book"
+            percent={43}
+            image={require("../assets/example_4.jpeg")}
+          />
+          <SmallBook
+            title="Your book title"
+            percent={78}
+            image={require("../assets/example_5.jpeg")}
+          />
         </div>
       </div>
     </div>
