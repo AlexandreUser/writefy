0675bcc4f00cdd81e225703090c1551d877f0ead
Alexandre Vieira <AlexandreV1@bitbucket.org>
2020-08-28 03:56:53 -0300

    added new frontend features

--- frontend/writefy/src/screen/indexScreen.js
+++ frontend/writefy/src/screen/indexScreen.js
@@ -20,14 +20,99 @@ export default function App(props) {
           <span style={{ fontWeight: "bold" }}>Popular</span> this week
         </p>
         <div className={styles.sectionContainer}>
-          <img
-            src={require("../assets/book_example_1.jpg")}
-            className={styles.boxContainer}
-          />
-          <img
-            src={require("../assets/book_example_2.jpg")}
-            className={styles.boxContainer}
-          />
+          <div style={{ marginLeft: "5px" }}>
+            <p className={styles.titleForImage}>Vanished into the woods</p>
+
+            <div className={styles.boxBehind}></div>
+            <img
+              src={require("../assets/exampleImage.jpg")}
+              width="25"
+              height="25"
+              className={styles.userLogoAbsolute}
+            />
+            <p className={styles.authorForImage}>@ John doe</p>
+
+            <img
+              src={require("../assets/book_example_1.jpg")}
+              className={styles.boxContainer}
+            />
+          </div>
+
+          <div style={{ marginLeft: "5px" }}>
+            <p className={styles.titleForImage}>Vanished into the woods</p>
+
+            <div className={styles.boxBehind}></div>
+            <img
+              src={require("../assets/exampleImage.jpg")}
+              width="25"
+              height="25"
+              className={styles.userLogoAbsolute}
+            />
+            <p className={styles.authorForImage}>@ John doe</p>
+
+            <img
+              src={require("../assets/book_example_1.jpg")}
+              className={styles.boxContainer}
+            />
+          </div>
+        </div>
+        <div className={styles.authorsOfTheWeek}>
+          <p style={{ color: "#545454", fontSize: "16px" }}>
+            <span style={{ fontWeight: "bold" }}>Authors</span> of the week
+          </p>
+          <div className={styles.topSection}>
+            <div className={styles.userContainer}>
+              <img
+                src={require("../assets/exampleImage.jpg")}
+                width="50"
+                height="50"
+                className={styles.userLogo}
+              />
+              <div>
+                <p className={styles.username}>@John Doe</p>
+                <p className={styles.level}>Level 15</p>
+              </div>
+            </div>
+            <div className={styles.userContainer}>
+              <img
+                src={require("../assets/exampleImage.jpg")}
+                width="50"
+                height="50"
+                className={styles.userLogo}
+              />
+              <div>
+                <p className={styles.username}>@John Doe</p>
+                <p className={styles.level}>Level 15</p>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.topSection}>
+            <div className={styles.userContainer}>
+              <img
+                src={require("../assets/exampleImage.jpg")}
+                width="50"
+                height="50"
+                className={styles.userLogo}
+              />
+              <div>
+                <p className={styles.username}>@John Doe</p>
+                <p className={styles.level}>Level 15</p>
+              </div>
+            </div>
+            <div className={styles.userContainer}>
+              <img
+                src={require("../assets/exampleImage.jpg")}
+                width="50"
+                height="50"
+                className={styles.userLogo}
+              />
+              <div>
+                <p className={styles.username}>@John Doe</p>
+                <p className={styles.level}>Level 15</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
